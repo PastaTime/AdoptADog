16c2a38bbd96577b32fde961ba68982c221052cf
Arran Davis <adav194@aucklanduni.ac.nz>
2019-01-26 00:23:45 -0800

    Add Start Menu

---
+++ AdoptADog/Assets/Scripts/UI/PlayerStartButton.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class PlayerStartButton : MonoBehaviour
+{
+    public PlayerStartManager manager;
+    public KeyCode buttonKey = KeyCode.A;
+    public bool buttonSelected = false;
+    
+    public float pulseTime = 1f;
+    private float _currentTime = 0f;
+    
+    
+    // Used for flashing the button.
+    private bool _buttonFlashState = true;
+    private Text _buttonText;
+
+    void Start()
+    {
+        _buttonText = GetComponent<Text>();
+        manager = FindObjectOfType<PlayerStartManager>();
+        manager.Register(this);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonSelected)
+        {
+            return;
+        }
+        
+        if (Input.GetKeyDown(buttonKey))
+        {
+            buttonSelected = true;
+            selectButton();
+        }
+        else
+        {
+            pulseButton();
+        }
+    }
+
+    public bool PlayerReady()
+    {
+        return buttonSelected;
+    }
+    
+
+    private void pulseButton()
+    {
+        _currentTime += Time.deltaTime;
+        if (_currentTime > pulseTime)
+        {
+            _currentTime = 0f;
+            toggleButton();
+        }
+    }
+
+    private void toggleButton()
+    {
+        _buttonFlashState = !_buttonFlashState;
+
+        if (_buttonFlashState)
+        {
+            _buttonText.text = "Press A";
+        }
+        else
+        {
+            _buttonText.text = "-------";
+        }
+    }
+
+    private void selectButton()
+    {
+        _buttonText.text = "Selected.";
+    }
+}
---
+++ AdoptADog/Assets/Scripts/UI/PlayerStartManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartManager : MonoBehaviour
+{
+    public List<PlayerStartButton> buttonList = new List<PlayerStartButton>();
+
+    void Update()
+    {
+        if (AllPlayersReady())
+        {
+            StartGame();
+        }
+        
+    }
+
+    public void Register(PlayerStartButton button)
+    {
+        buttonList.Add(button);
+    }
+
+    private bool AllPlayersReady()
+    {
+        foreach (PlayerStartButton button in buttonList)
+        {
+            if (!button.PlayerReady())
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+
+    private void StartGame()
+    {
+        Debug.Log("start game!");
+        Console.WriteLine("START GAME!");
+    }
+    
+}

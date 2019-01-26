40a846a7fe848709ba5382c03c6ca441e8c76f14
Arran Davis <adav194@aucklanduni.ac.nz>
2019-01-26 15:23:09 -0800

    Adds dynamic score bar in HUD

---
+++ AdoptADog/Assets/Scripts/UI/PlayerScoreBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreBar : MonoBehaviour
+{
+    public int playerNumber = 1;
+    
+    public Color fillColor = Color.red;
+    
+    public float maxPoints = 100f;
+    public float minXPosition = -6f;
+    public float maxXPosition = 6f;
+
+    public Transform bar;
+    
+    private float _currentPoints;
+    
+    void Start()
+    {
+        UpdatePoints(10f);
+        bar.GetComponent<SpriteRenderer>().color = fillColor;
+    }
+
+    public void UpdatePoints(float points)
+    {
+        _currentPoints = points;
+        var barFill = _currentPoints / maxPoints;
+        Debug.Log(barFill);
+        var scale = bar.localScale;
+        scale.x = barFill * (maxXPosition - minXPosition);
+        Debug.Log(scale.x);
+        bar.localScale = scale;
+
+        var pos = bar.transform.localPosition;
+        pos.x = minXPosition + 0.5f * scale.x;
+        Debug.Log(pos.x);
+        bar.transform.localPosition = pos;
+    }
+    
+}

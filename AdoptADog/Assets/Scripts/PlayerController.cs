6bf98901b4d8c0adc0bdc67b9039194ca318654d
Reuben Rakete <reuben@rakete.co.nz>
2019-01-26 16:18:21 -0800

    Add AI controller

--- AdoptADog/Assets/Scripts/PlayerController.cs
+++ AdoptADog/Assets/Scripts/PlayerController.cs
@@ -4,87 +4,19 @@ using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Dog))]
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10;
-    public float acceleration = 0.8f;
-
     public int playerNumber = 1;
-
-    private bool _rolling;
-    public bool Rolling
-    {
-        get => _rolling;
-
-        private set
-        {
-            _spriteRenderer.flipY = value;
-            gameObject.layer = value ? LayerMask.NameToLayer("Dodge") : LayerMask.NameToLayer("Dog");
-            _rolling = value;
-            _animator.SetBool(RollingAnimationId, value);
-        }
-    }
-
-    private bool _leaping;
-    public bool Leaping
-    {
-        get => _leaping;
-        private set
-        {
-            _leaping = value;
-            _animator.SetBool(LeapingAnimationId, value);
-        }
-    }
-
-    public Rigidbody2D Rigidbody { get; private set; }
-    public Collider2D PhysicsCollider { get; private set; }
-    private SpriteRenderer _spriteRenderer;
-    private Animator _animator;
-
-    private readonly List<GameObject> _handledCollisions = new List<GameObject>();
-
-    private readonly PlayerAction _roll = new PlayerAction()
-    {
-        ActionTime = 0.6f,
-        Cooldown = 1f,
-        SpeedMultiplier = 1.2f,
-    };
-
-    private readonly PlayerAction _leap = new PlayerAction()
-    {
-        ActionTime = 0.3f,
-        Cooldown = 0.7f,
-        SpeedMultiplier = 1.5f,
-    };
-
-    private static readonly int RollingAnimationId = Animator.StringToHash("Rolling");
-    private static readonly int LeapingAnimationId = Animator.StringToHash("Leaping");
-    private static readonly int VelocityAnimationId = Animator.StringToHash("Velocity");
-
-    public void SetCollisionHandled(PlayerController other)
-    {
-        _handledCollisions.Add(other.gameObject);
-    }
+    private Dog _dog;
 
     private void Start()
     {
-        Rigidbody = GetComponent<Rigidbody2D>();
-        PhysicsCollider = GetComponent<Collider2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _animator = GetComponent<Animator>();
+        _dog = GetComponent<Dog>();
     }
 
-    private bool CanDoAction(PlayerAction action = null)
+    private Vector2 GetMovementDir()
     {
-        if (Rolling || Leaping) return false;
-        if (action == null) return true;
-        return Time.time > action.TimeFinished + action.Cooldown;
-    }
-
-    private Vector2 GetMovementDir() {
         float moveHorizontal = Controller.getSingleton().getHorizontal(playerNumber);
         float moveVertical = Controller.getSingleton().getVertical(playerNumber);
 
@@ -93,122 +25,17 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        _handledCollisions.Clear();
-
         Vector2 movementDir = GetMovementDir();
+        _dog.MovementDir = movementDir;
 
         if (Controller.getSingleton().getA(playerNumber))
         {
-            Roll();
+            _dog.Roll();
         }
         if (Controller.getSingleton().getB(playerNumber))
         {
-            Leap();
-        }
-
-        if (CanDoAction())
-        {
-            Vector2 idealSpeed = movementDir * speed;
-            Rigidbody.velocity = Vector2.Lerp(idealSpeed, Rigidbody.velocity, acceleration);
-        }
-
-        if (Rigidbody.velocity.x == 0)
-        {
-            //  nothing
-        }
-        else if (Rigidbody.velocity.x > 0)
-        {
-            _spriteRenderer.flipX = true;
-        }
-        else
-        {
-            _spriteRenderer.flipX = false;
-        }
-
-        _animator.SetFloat(VelocityAnimationId, Rigidbody.velocity.magnitude);
-    }
-
-    private void Roll()
-    {
-        if (!CanDoAction(_roll)) return;
-        Rolling = true;
-
-        Rigidbody.velocity = GetMovementDir() * speed * _roll.SpeedMultiplier;
-
-        StartCoroutine(RollRoutine());
-    }
-
-    private void Leap()
-    {
-        if (!CanDoAction(_leap)) return;
-        Leaping = true;
-
-        Rigidbody.velocity = Rigidbody.velocity.normalized * speed * _leap.SpeedMultiplier;
-
-        StartCoroutine(LeapRoutine());
-    }
-
-    private IEnumerator RollRoutine()
-    {
-        _roll.TimeStarted = Time.time;
-        yield return new WaitForSeconds(_roll.ActionTime);
-        _roll.TimeFinished = Time.time;
-        Rolling = false;
-    }
-
-    private IEnumerator LeapRoutine()
-    {
-        yield return new WaitForSeconds(_leap.ActionTime);
-        Leaping = false;
-        _leap.TimeFinished = Time.time;
-    }
-
-    private void HandleTriggerCollision(PlayerController other)
-    {
-        if (other.Rolling) return;
-        if (!Rolling) return;
-
-        float v1 = Vector2.Dot(other.Rigidbody.velocity, Rigidbody.velocity);
-        float v2 = Rigidbody.velocity.magnitude;
-        float relativeVelocity = v2 - v1;
-        float timeLeft = _roll.ActionTime - (Time.time - _roll.TimeStarted);
-
-        // If there's not enough time to pass the player, stop rolling
-        if (Math.Abs(relativeVelocity) * timeLeft < other.PhysicsCollider.bounds.size.x + PhysicsCollider.bounds.size.x)
-        {
-            Rolling = false;
-            _roll.TimeFinished = Time.time;
-            StopCoroutine(RollRoutine());
+            _dog.Leap();
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        // Only handle player collisions
-        var otherPlayer = other.gameObject.GetComponent<PlayerController>();
-        if (otherPlayer)
-        {
-            // Don't handle collisions twice
-            if (_handledCollisions.Contains(other.gameObject)) return;
-            CollisionUtils.HandlePlayerCollision(this, otherPlayer);
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        var otherPlayer = other.gameObject.GetComponent<PlayerController>();
-        if (otherPlayer)
-        {
-            HandleTriggerCollision(otherPlayer);
-        }
-    }
-}
-
-
-public class PlayerAction
-{
-    public float ActionTime { get; set; }
-    public float SpeedMultiplier { get; set; }
-    public float Cooldown { get; set; }
-    public float TimeStarted { get; set; } = float.MinValue;
-    public float TimeFinished { get; set; } = float.MinValue;
 }
